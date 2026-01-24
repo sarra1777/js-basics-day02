bae8fc72947884e65bbddb64877813a4941d9c9f
sarra1777 <sarranaddari568@gmail.com>
2026-01-24 12:16:49 +0100

    Add files via upload

---
+++ day01/basics js/basics.js
@@ -0,0 +1,19 @@
+let firstaname = 'sarra';
+let lastname = 'naddari';
+console.log('your name is ' + firstaname + ' and your lastname is ' + lastname);
+let favoritecolor = 'pink';
+console.log('your favorite color is ' + favoritecolor);
+
+let email = 'sarranaddari@gmail.com';
+console.log('your email is ' + email);
+
+let online = false ;
+let forsale = true;
+console.log(typeof online);
+console.log(`is sarra is online : ${online}`);
+
+console.log(`is this car for sale ?: ${forsale}`);
+
+
+window.alert('this is an alert box !');
+console.log('hello');
